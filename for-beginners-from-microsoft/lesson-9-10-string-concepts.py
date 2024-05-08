25c9bcd1d5cbc004be1925834a8184d8e0c68329
yaroslav@wavedecompiler <101848668+yaroslavgladkov@users.noreply.github.com>
2024-05-08 16:09:01 -0400

    update

--- for-beginners-from-microsoft/lesson-9-10-string-concepts.py
+++ for-beginners-from-microsoft/lesson-9-10-string-concepts.py
@@ -1,3 +1,3 @@
 first_name = input('what is your first name? ')
 last_name = input('what is your last name? ')
-print('hello ' + first_name + '' + last_name)
+print('hello ' + first_name.capitalize() + '' + last_name.capitalize())
