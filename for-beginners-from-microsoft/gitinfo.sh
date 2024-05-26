e9fcbb6449ad906dddd38d2447fb0913c013159c
yaroslav <101848668+yaroslavgladkov@users.noreply.github.com>
2024-05-26 04:21:11 +0200

    update

---
+++ for-beginners-from-microsoft/gitinfo.sh
@@ -0,0 +1,16 @@
+echo "the following commands will be executed:
+git status
+git branch -a
+git remote -v
+git config -l
+git rev-parse --git-dir --git-work-tree --absolute-git-dir --absolute-git-w>
+git show --format=fuller
+git ls-remote --heads --tags origin
+"
+git status
+git branch -a
+git remote -v
+git config -l
+git rev-parse --git-dir --git-work-tree --absolute-git-dir --absolute-git-work-tree
+git show --format=fuller
+git ls-remote --heads --tags origin
---
+++ for-beginners-from-microsoft/lesson-11-12-string-format.py
@@ -0,0 +1,2 @@
+first_name = input('enter your first name: ')
+print('hello, {}'.format(first_name.capitalize()))
