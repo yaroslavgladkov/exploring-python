5555270d39a535c8399c034b9e697be0e047edf2
yaroslav <101848668+yaroslavgladkov@users.noreply.github.com>
2024-05-26 12:06:57 -0700

    update gitinfo.sh

--- for-beginners-from-microsoft/lesson-11-12-formatting-strings.py
+++ for-beginners-from-microsoft/lesson-11-12-formatting-strings.py
@@ -2,3 +2,11 @@ first_name = input('enter your first name: ')
 last_name = input('enter your last name: ')
 
 print('hello, {} {}'.format(first_name.capitalize(), last_name.capitalize()))
+print('hello, {0} {1}'.format(first_name.capitalize(), last_name.capitalize()))
+print('hello, {} {}'.format(first_name.capitalize(), last_name.capitalize()))
+
+output = 'Hello, ' + first_name + ' ' + last_name
+output = 'Hello, {} {}'.format(first_name, last_name)
+output = 'Hello, {0} {1}'.format(first_name, last_name)
+# Only available in Python 3
+output = f'Hello, {first_name} {last_name}'
