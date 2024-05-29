45ae2cb8e570830cc50ab57f656ca0b76a81b26a
yaroslav <101848668+yaroslavgladkov@users.noreply.github.com>
2024-05-29 09:08:36 -0700

    update

---
+++ for-beginners-from-microsoft/13-14-numeric-data-types.py
@@ -0,0 +1,19 @@
+pi = 3.14159
+print(pi)
+
+first_num = input('please enter first number: ')
+second_num = input('please enter second number: ')
+print('+ ', int(first_num) + int(second_num))
+print('- ', int(first_num) - int(second_num))
+print('* ', int(first_num) * int(second_num))
+print('/ ', int(first_num) / int(second_num))
+print('** ', int(first_num) ** int(second_num))
+
+print('+ ', float(first_num) + float(second_num))
+print('- ', float(first_num) - float(second_num))
+print('* ', float(first_num) * float(second_num))
+print('/ ', float(first_num) / float(second_num))
+print('** ', float(first_num) ** float(second_num))
+
+days_in_February = 28
+print(str(days_in_February) + ' total days in February')
