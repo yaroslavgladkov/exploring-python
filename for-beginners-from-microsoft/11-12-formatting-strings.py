8635058031c6c9ab402228a1ee91c602dabbb576
yaroslav <101848668+yaroslavgladkov@users.noreply.github.com>
2024-05-29 07:54:12 -0700

    update

--- for-beginners-from-microsoft/11-12-formatting-strings.py
+++ for-beginners-from-microsoft/11-12-formatting-strings.py
@@ -6,7 +6,11 @@ print('hello, {0} {1}'.format(first_name.capitalize(), last_name.capitalize()))
 print('hello, {} {}'.format(first_name.capitalize(), last_name.capitalize()))
 
 output = 'Hello, ' + first_name + ' ' + last_name
+print(output)
 output = 'Hello, {} {}'.format(first_name, last_name)
-output = 'Hello, {0} {1}'.format(first_name, last_name)
+print(output)
+output = 'Hello, {1} {0}'.format(first_name, last_name)
+print(output)
 # Only available in Python 3
 output = f'Hello, {first_name} {last_name}'
+print(output)
