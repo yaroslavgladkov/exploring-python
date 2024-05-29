62a4f3a22ad6aed62add770326ed8b495f6500e2
yaroslav <101848668+yaroslavgladkov@users.noreply.github.com>
2024-05-29 19:15:43 +0200

    update

--- for-beginners-from-microsoft/15-16-date-data-type.py
+++ for-beginners-from-microsoft/15-16-date-data-type.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta
 
-birthday= input('When is your birthday (dd/mm/yyyy)?')
+birthday = input('When is your birthday (dd/mm/yyyy)?')
 birthday_date = datetime.strptime(birthday, '%d/%m/%Y')
 print ('Birthday:' + str(birthday_date))
 one_day = timedelta(days=1)
