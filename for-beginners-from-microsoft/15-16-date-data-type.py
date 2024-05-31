b17f5c25544483e4b0c0d28409388c72c978a183
yaroslav <101848668+yaroslavgladkov@users.noreply.github.com>
2024-05-31 11:37:47 -0700

    update

--- for-beginners-from-microsoft/15-16-date-data-type.py
+++ for-beginners-from-microsoft/15-16-date-data-type.py
@@ -1,10 +1,11 @@
 from datetime import datetime, timedelta
 
-birthday = input('When is your birthday (dd/mm/yyyy)? ')
+birthday = '11/11/1111'
+#birthday = input('When is your birthday (dd/mm/yyyy)? ')
 
 birthday_date = datetime.strptime(birthday, '%d/%m/%Y')
 
-print ('Birthday:' + str(birthday_date))
+print('Birthday:' + str(birthday_date))
 
 one_day = timedelta(days=1)
 birthday_eve = birthday_date - one_day
@@ -13,3 +14,7 @@ print('Day before birthday: ' + str(birthday_eve))
 one_week = timedelta(weeks=1)
 birthday_eve = birthday_date - one_week
 print('Week before birthday: ' + str(birthday_eve))
+
+now = datetime.now()
+
+print(f'Day: {now.day}, Hour: {now.hour}, Minute: {now.minute}')
